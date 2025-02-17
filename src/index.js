82e9712f7a0b8bad25897a3d4d4d165f3abf9d1f
Andreea <andreeapuiu789@yahoo.com>
2025-02-17 16:59:49 +0200

    Prima etapa

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React, { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { createRoot } from "react-dom/client"; //React’s library to talk to web browsers
 import "./styles.css";
 
 import App from "./App";
